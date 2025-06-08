7a96c016d08f5f90ae24cd6336523f42a5e9d1f2
cotehila <154602021+cotehila@users.noreply.github.com>
2025-06-08 13:33:37 +0300

    Update funcProdTrig.sql

--- DBProject/שלב ד/funcProdTrig.sql
+++ DBProject/שלב ד/funcProdTrig.sql
@@ -5,21 +5,7 @@
 
 -- ========= FUNCTIONS =========
 
--- Function 1: Return all purchases of a given customer
-CREATE OR REPLACE FUNCTION get_customer_purchases(p_cid INT)
-RETURNS refcursor AS $$
-DECLARE
-    cur refcursor;
-BEGIN
-    OPEN cur FOR
-        SELECT purid, purdate, purcost
-        FROM purchase
-        WHERE cid = p_cid;
-    RETURN cur;
-END;
-$$ LANGUAGE plpgsql;
-
--- Function 2 (Fixed): Check product availability per supplier
+-- Function 1: Check product availability per supplier
 CREATE OR REPLACE FUNCTION check_product_availability(p_pid INT)
 RETURNS TABLE(sname character varying, sid integer, total_amount bigint) AS $$
 BEGIN
@@ -34,6 +20,40 @@ BEGIN
     GROUP BY s.sname, s.sid;
 END;
 $$ LANGUAGE plpgsql;
+
+-- Function 2: Return all purchases of a given customer
+CREATE OR REPLACE FUNCTION get_customer_purchases(p_cid INT)
+RETURNS refcursor AS $$
+DECLARE
+    cur refcursor;
+BEGIN
+    OPEN cur FOR
+        SELECT purid, purdate, purcost
+        FROM purchase
+        WHERE cid = p_cid;
+    RETURN cur;
+END;
+$$ LANGUAGE plpgsql;
+
+--ANOTHER FUNCTION:
+-- פונקציה שמקבלת מזהה מוצר ומדפיסה את שמו והכמות במלאי
+CREATE OR REPLACE FUNCTION print_product_info(p_pid INT)
+RETURNS VOID AS $$
+DECLARE
+    v_name VARCHAR;
+    v_quantity INT;
+BEGIN
+    -- שליפת שם המוצר והכמות למשתנים מקומיים (קורסור מרומז)
+    SELECT pname, quantity
+    INTO v_name, v_quantity
+    FROM product
+    WHERE pid = p_pid;
+
+    -- הדפסת המידע על המוצר
+    RAISE NOTICE 'Product: %, Quantity: %', v_name, v_quantity;
+END;
+$$ LANGUAGE plpgsql;
+
 
 -- ========= PROCEDURES =========
 
